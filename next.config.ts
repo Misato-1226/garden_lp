9485b4c55a6ebc6b61fb6f1a7f6449fbc6c726ba
Misato <rexlove1226@gmail.com>
2025-01-14 12:44:35 +0900

    change next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  experimental: {
+    appDir: true, // 必要に応じて設定
+  },
+};
 
 export default nextConfig;
